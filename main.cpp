bd6712943ca201712f8100d8ff443ea7f5c650c6
kelvinngure <njuguna.kelvin22@gmail.com>
2021-12-22 19:39:12 -0500

    color insertion example

--- main.cpp
+++ main.cpp
@@ -11,7 +11,16 @@ int main(){
         // header info
         image << "P3" << endl;       // P3 is the PPM type. 
         image << "250 250" << endl;  // 250 by 250 dimensions
-        image << "255" << endl;      // max value for each color 
+        image << 255 << endl;      // max value for each color 
+    }
+
+    // loop through to setup rgb values per row
+    
+    for(int i = 0; i < 250; i++){
+        for(int i = 0; i < 250; i++){
+            // all white image. remember the spacing
+            image << "255 255 255" << endl; // all black -> image << 0 << " " << 0 << " " << 0 << " " << endl;
+        }
     }
 
     image.close();
