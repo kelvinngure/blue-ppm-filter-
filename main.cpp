86e3ca27d5b7e06f4d7aab68c95182e1b749a91c
kelvinngure <njuguna.kelvin22@gmail.com>
2021-12-22 20:13:15 -0500

    sandbox: copying image, single rgb pixel

--- main.cpp
+++ main.cpp
@@ -1,29 +1,54 @@
 #include <fstream> // we want to insert into the output stream which is our image file
+#include <iostream>
 
 using namespace std;
 
 int main(){
+    // EXPERIMENTAL STUFF AT BOTTOM COMMENTS
 
-    ofstream image;
-    image.open("image.ppm");
+    ifstream old_image;
+    ofstream new_image;
 
-    if(image.is_open()){
-        // header info
-        image << "P3" << endl;       // P3 is the PPM type. 
-        image << "250 250" << endl;  // 250 by 250 dimensions
-        image << 255 << endl;      // max value for each color 
-    }
+    old_image.open("monument.ppm");
+    new_image.open("monument_copy.ppm");
 
-    // loop through to setup rgb values per row
-    
-    for(int i = 0; i < 250; i++){
-        for(int i = 0; i < 250; i++){
-            // all white image. remember the spacing
-            image << "255 255 255" << endl; // all black -> image << 0 << " " << 0 << " " << 0 << " " << endl;
+    string tempStorageString;
+
+    // direct copy example
+    if(old_image.is_open() && new_image.is_open()){
+        while(getline(old_image, tempStorageString)){
+            new_image << tempStorageString << endl;
         }
     }
-
-    image.close();
-
+ 
     return 0;
-}
+}   
+
+
+
+
+
+    // ofstream image;
+    // image.open("image.ppm");
+
+    // if(image.is_open()){
+    //     // header info
+    //     image << "P3" << endl;       // P3 is the PPM type. 
+    //     image << "250 250" << endl;  // 250 by 250 dimensions
+    //     image << 255 << endl;      // max value for each color 
+    // }
+
+    // /* loop through to setup rgb values per row
+    //     for(int i = 0; i < 250; i++){
+    //         for(int i = 0; i < 250; i++){
+    //             // all white image. remember the spacing
+    //             image << "255 255 255" << endl; // all black -> image << 0 << " " << 0 << " " << 0 << " " << endl;
+    //         }
+    //     }
+    // */  
+    // image.close();
+
+
+// footnote: 
+// cin >> name;        means extract from cin (input stream) into name
+// cout << "hello";    means insert into cout(output stream) value "hello"
